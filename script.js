7b87e7ef98b334d516c027bd65011536fcdd5991
ryan0583 <36458096+ryan0583@users.noreply.github.com>
2018-02-19 23:04:31 +0000

    Changed position of picture

--- script.js
+++ script.js
@@ -28,10 +28,10 @@ function update()
       var top = thisOffset.top;
       var height = $(this).height();
 
-      var newPos = Math.round(top - 140 - winTop * velocity);
+      var newPos = Math.round((top - 60 - winTop) * velocity);
 
       $(this).css('backgroundPosition', '100% ' + newPos +  'px');
-      $(this).css('backgroundSize', 'auto ' + Math.round(0.7 * winHeight) + 'px');
+      $(this).css('backgroundSize', 'auto ' + Math.round(0.5 * winHeight) + 'px');
       /*$(this).css('height', Math.round(0.3 * winHeight) + 'px');*/
       /*$(this).css('padding-top', Math.round(0.1 * winHeight) + 'px');*/
     });
